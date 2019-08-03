256e675e28dec4d0fa2836e3605d3d94716be659
Julien LAVIE <jlavie@leadformance.com>
2019-08-03 22:38:38 +0200

    feat(theme): initialize theme

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jlavie
+ * Date: 03/08/2019
+ * Time: 22:20
+ */
+
+wp_footer();
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jlavie
+ * Date: 03/08/2019
+ * Time: 22:20
+ */
+
+get_header();
+
+get_footer();
