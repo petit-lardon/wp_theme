6a8054ed6dc237c1d4cfbd4625e7c9be3077effb
Julien LAVIE <jlavie@leadformance.com>
2019-08-04 23:27:07 +0200

    feat(admin): create simple form to custom fields in admin panel

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -23,10 +23,15 @@ function folio_add_admin_page() {
     $sub_menu_css = 'css';
     $sub_menu_css_function = 'myfolio_theme_css_page';
 
+
+
     add_menu_page($page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position);
 
     add_submenu_page($menu_slug, $menu_title, $sub_menu_settings, $capability, $menu_slug,$function);
     add_submenu_page($menu_slug, $menu_title.' '.$sub_menu_css, $sub_menu_css, $capability, $menu_slug.'_'.$sub_menu_css,$sub_menu_css_function);
+
+    //action dans la création de la page. si page pas créée, la fonction suivante ne sera pas appelée
+    add_action('admin_init', 'myfolio_custom_settings');
 }
 
 //afficher notre page dans l'admin menu
@@ -38,4 +43,21 @@ function myfolio_theme_create_page() {
 
 function myfolio_theme_css_page() {
     echo '<h1>MyFolio css page</h1>';
+}
+
+function myfolio_custom_settings() {
+    register_setting('myfolio_settings_group', 'first_name');
+
+    add_settings_section('myfolio-sidebar-options', 'Sidebar options', 'myfolio_sidebar_options', 'myfolio');
+
+    add_settings_field('sidebar-name', 'First name', 'myfolio_sidebar_name', 'myfolio', 'myfolio-sidebar-options');
+}
+
+function myfolio_sidebar_options() {
+    echo 'Customize your sidebar informations';
+}
+
+function myfolio_sidebar_name() {
+    $firstName = esc_attr(get_option('first_name'));
+    echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="first name" />';
 }
--- inc/templates/myFolio-admin.php
+++ inc/templates/myFolio-admin.php
@@ -1,4 +1,11 @@
 <h1>MyFolio settings page</h1>
-<h3>Manage options</h3>
+<?php settings_errors(); ?>
 
+<form method="post" action="options.php">
+    <?php
+        settings_fields('myfolio_settings_group');
+        do_settings_sections('myfolio');
+        submit_button();
+    ?>
+</form>
 <?php bloginfo(); ?>
