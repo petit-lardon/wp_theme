f541a9ada2e7f528bd541733ee530203eab4e1d1
Julien LAVIE <jlavie@leadformance.com>
2019-08-07 23:44:41 +0200

    feat(picture): connect wp media uploader to the button

--- js/myfolio.admin.js
+++ js/myfolio.admin.js
@@ -1,4 +1,20 @@
 jQuery(document).ready(function($) {
     var mediaUploader;
 
+    $('#upload-button').click(function(e) {
+        e.preventDefault();
+
+        if(mediaUploader) {
+            mediaUploader.open();
+            return;
+        }
+
+        mediaUploader = wp.media.frames.file_frame = wp.media({
+            title: 'Profile picture',
+            button: {
+                text: 'Choose a picture'
+            },
+            multiple: false
+        });
+    });
 });
