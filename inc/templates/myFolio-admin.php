ada1622754c970b0fd6060c7db9599345701742c
Julien LAVIE <jlavie@leadformance.com>
2019-08-07 22:36:26 +0200

    feat(style): add admin stylesheet with scss

--- inc/templates/myFolio-admin.php
+++ inc/templates/myFolio-admin.php
@@ -1,7 +1,27 @@
 <h1>MyFolio settings page</h1>
-<?php settings_errors(); ?>
+<?php
+    settings_errors();
 
-<form method="post" action="options.php">
+    $firstName = esc_attr(get_option('first_name'));
+    $lastName = esc_attr(get_option('last_name'));
+    $fullName = $firstName.' '.$lastName;
+?>
+
+<div class="myfolio__preview">
+    <div class="myfolio__preview__sidebar">
+        <h1 class="myfolio__preview__sidebar__username">
+            <?php print $fullName ?>
+        </h1>
+        <h2 class="myfolio__preview__sidebar__description">
+            <?php print $userDescription = esc_attr(get_option('user_description')); ?>
+        </h2>
+        <div class="myfolio__preview__sidebar__icon-wrapper">
+
+        </div>
+    </div>
+</div>
+
+<form method="post" action="options.php" class="myfolio__form">
     <?php
         settings_fields('myfolio_settings_group');
         do_settings_sections('myfolio');
